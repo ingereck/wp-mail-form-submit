014ea1d9d37b8103081a4a5d3a676cfc36c7807f
Inge Reck <info@ingereck.net>
2020-08-01 20:46:32 +0200

    Update wp-mail-form-submit.php

--- wp-mail-form-submit.php
+++ wp-mail-form-submit.php
@@ -9,15 +9,13 @@
  * @wordpress-plugin
  * Plugin Name: wp_mail Form Submit
  * Description: Enables to send Email via SMTP
- * Plugin URI:  https://gist.github.com/ingereck
+ * Plugin URI:  https://github.com/ingereck/wp-mail-form-submit.git
  * Version:     0.1.0
  * Author:      Inge Reck
  * Author URI:  https://ingereck.net
  * License:     GNU General Public License v2 (or later)
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
-
-namespace wp_mail_form_submit;
 
 
 // exit if file is called directly
